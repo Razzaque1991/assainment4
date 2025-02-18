89a8025ef4ef7683a975b851850752495819a655
Abdur Razzaque <razzaque.0011@gamail.com>
2025-02-18 16:25:56 +0000

    assainment_task_05_done & validproposole corection

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 
-
-// task 01----------------
-
 function calculateVAT( price ) {
 
     if (typeof price === "number" && price > 0) {
@@ -13,7 +10,6 @@ function calculateVAT( price ) {
     }
 }
 
-// task 02--------------
 
 function  validContact( contact ) {
     if (typeof contact === "object") {
@@ -30,10 +26,6 @@ function  validContact( contact ) {
 
     return false;
 }
-
-
-
-// task 03--------------
 
 
 function willSuccess(marks) {
@@ -59,19 +51,38 @@ function willSuccess(marks) {
     return exam(fiftyOver, fiftyUnder);
 }
 
-// task 04
-
  
-function  validProposal( person1 , person2 ) {
-      if (typeof person1 !== "object" || typeof person2 !== "object") {
+
+function validProposal(person1, person2) {
+    if (typeof person1 !== "object" || typeof person2 !== "object") {
         return "Invalid"; 
     }
-
-    if (Math.abs(person1.age - person2.age) === 7 &&
+    let ageDiff = Math.abs(person1.age - person2.age);
+    if (ageDiff <= 7 &&
         ((person1.gender === "male" && person2.gender === "female") || 
          (person1.gender === "female" && person2.gender === "male"))) {
         return true;
     }
 
-    return false;
+    return "Invalid";
 }
+
+ 
+function calculateSleepTime(times) {
+    if (!Array.isArray(times) || !times.every(time => typeof time === "number")) {
+        return "Invalid";
+    }
+    let sum = 0;
+    for (const time of times) {
+        sum += time;
+    }
+    let totalSecond = sum * 60 ;
+    let hours = Math.floor(totalSecond / 3600);
+    let minutes = Math.floor((totalSecond % 3600) / 60);
+    let seconds = totalSecond % 60;
+
+    let totalTime = { hour: hours, minute: minutes, second: seconds };
+    return totalTime;
+}
+
+
